152c0a6f2523b598abed184fee372d1d2ed24042
Andrey Kushch <droni2442@gmail.com>
2019-11-24 23:50:03 +0200

    first commit

---
+++ src/component/SectionFirst/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function SectionFirst() {
+  return (
+    <div className='section-first'>
+      SECTION FIRST
+    </div>
+  );
+}
+
+export default SectionFirst;
---
+++ src/component/SectionSecond/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function SectionSecond() {
+  return (
+    <div className='section-second'>
+      SECTION SECOND
+    </div>
+  );
+}
+
+export default SectionSecond;
